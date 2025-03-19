362052ad5183b21f31e32321daf1e66bc0ea5acc
kravchenski <max5045955@gmail.com>
2025-03-19 21:26:14 +0100

    init: create func for gdansk with logging

---
+++ gdansk.py
@@ -0,0 +1,33 @@
+import asyncio
+import logging
+import os
+import time
+
+from art import tprint
+
+from utils.available_dates import available_dates
+from utils.utils import clear_console_no_dates
+
+tprint("Online Registration", font="cybermedum")
+tprint("Gdansk", font="cybermedum")
+time.sleep(5)
+os.system('cls')
+
+while True:
+    print("Fetching dates...")
+    status_code, dates_for_wniosek = asyncio.run(
+        available_dates('https://kolejka.gdansk.uw.gov.pl/admin/API/date/5/304/pl'))
+    if dates_for_wniosek:
+        print(dates_for_wniosek)
+    else:
+        clear_console_no_dates()
+    logging.basicConfig(
+        filename="gdansk.log",
+        filemode='a',
+        level=logging.INFO
+    )
+
+    logger = logging.getLogger()
+
+    logger.info(
+        f"{logging.Formatter('%(asctime)s').format(logging.LogRecord(None, None, None, None, None, None, None))} | Статус: {status_code}, Данные: {dates_for_wniosek}")
